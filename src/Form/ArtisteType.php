df387fadd3034fd32b724b5e553268bd0d71a826
Jérôme magnier <craozor@gmail.com>
2022-04-19 16:01:28 +0200

    first commit

---
+++ src/Form/ArtisteType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artiste;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class ArtisteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'prenom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('birthdate', DateType::class, [
+                'label' => 'Date de naissance',
+                'years' => range(1900,2022),
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('imageFile',VichImageType::class, [
+                'required' => false,
+                'imagine_pattern' => 'my_thumb',
+                'label' => 'Photo',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'allow_delete' => false,
+                'download_uri' => false
+            ])
+            ->remove('updatedAt')
+            ->remove('movies')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Artiste::class,
+        ]);
+    }
+}
